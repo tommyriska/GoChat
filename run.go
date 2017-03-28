5ad6315169ed70b937caea887ef982b8d26694ea
aadneja <aadne.jacobsen@gmail.com>
2017-03-28 22:33:04 +0200

    server with encryption

--- run.go
+++ run.go
@@ -4,41 +4,58 @@ import (
 	"bufio"
 	"fmt"
 	"net"
-	"os"
 )
 
-func main() {
-	var key string
-	// connect to server
-	conn, _ := net.Dial("tcp", "158.37.63.27:8081")
+var clients []Client
 
-	// get key
-	key, _ = bufio.NewReader(conn).ReadString('\n')
-	fmt.Print("Key: " + key)
+type Client struct {
+	connection net.Conn
+	key        []byte
+}
 
-	// get key
-	keyMsg := []byte(key)
-	byteKey := keyMsg[0 : len(keyMsg)-1]
+func (c *Client) send(message []byte) {
+	c.connection.Write(message)
+}
 
-	// start listener thread
-	go listener(conn, byteKey)
-
+func (c *Client) listener() {
+	// send key
+	c.send([]byte(string(c.key) + "\n"))
+	// listen for msg
 	for {
-		// read in input from stdin
-		reader := bufio.NewReader(os.Stdin)
-		text, _ := reader.ReadString('\n')
-		cryptText := encrypt(byteKey, text)
-
-		// send to socket
-		fmt.Fprintf(conn, cryptText+"\n")
+		message, _ := bufio.NewReader(c.connection).ReadString('\n')
+		if len(message) > 0 {
+			msg := message[0 : len(message)-1]
+			fmt.Println(c.connection.RemoteAddr().String() + ": " + msg)
+			fmt.Print(c.connection.RemoteAddr().String() + ": " + decrypt(c.key, msg))
+			for _, element := range clients {
+				if element.connection != c.connection {
+					element.send([]byte(message + "\n"))
+				}
+			}
+		}
 	}
 }
 
-func listener(conn net.Conn, key []byte) {
+func (c *Client) startThread() {
+	go c.listener()
+}
+
+func main() {
+	// start server
+	port := ":8081"
+	ln, _ := net.Listen("tcp", port)
+	fmt.Println("Server is listening on " + port)
+
+	// generate key
+	key := createKey()
+
+	// accept clients
 	for {
-		// listen for message from server
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		msg := decrypt(key, message)
-		fmt.Print(msg)
+		conn, _ := ln.Accept()
+		newClient := Client{connection: conn, key: key}
+		clients = append(clients, newClient)
+		fmt.Println(conn.RemoteAddr().String(), "connected.")
+		fmt.Println("Connected clients:", len(clients))
+		newClient.startThread()
 	}
 }
