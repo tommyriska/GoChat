8bf6a098ed37d307a8a4d0a53d8c2faf2df7d79e
torasku <tor.arve.skutlaberg@gmail.com>
2017-05-22 17:32:01 +0200

    Update incl encrypt/decrypt

--- client_test.go
+++ client_test.go
@@ -4,6 +4,29 @@ import (
 	"testing"
 )
 
-func TestBase(t *testing.T) {
-	main()
+/* test will check if a string variable is altered after
+method call (encrypted) */
+func TestEncrypt(t *testing.T) {
+
+	key := make([]byte, 32)
+	var crypt string = "test crypto"
+	text := encrypt(key, crypt)
+
+	if text == crypt {
+		t.Error("Expected new text, got same.")
+	}
 }
+
+/* test will check if a string variable is altered after
+method call (decrypted) */
+func TestDecrypt(t *testing.T) {
+
+	key := make([]byte, 32)
+	var crypt string = "eL7Abmpadmk8nAaVcpT-a6MziwEKcL5z3ifS54SsnxA="
+	text := decrypt(key, crypt)
+
+	if text == crypt {
+		t.Error("Expected new text, got same")
+	}
+}
+
