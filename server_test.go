398c6f104ed52a577ac9f41e4aba512e5bbe2bf1
torasku <tor.arve.skutlaberg@gmail.com>
2017-05-29 18:32:55 +0200

    Added test for adding rooms

--- server_test.go
+++ server_test.go
@@ -80,3 +80,21 @@ func TestLoadRooms(t *testing.T) {
 		fmt.Println("Expected populated array, got an empty.")
 	}
 }
+
+/* test will check if a new room is added to lobby array */
+func TestSaveRoom(t *testing.T) {
+
+	var s string = "testName"
+	var w string = "testMsg"
+	var d string = "testDesc"
+
+	saveRoom(s, w, d)
+	testList := loadRooms()
+
+	for i := range testList {
+		if testList[i].name == s {
+			fmt.Println("New room succesfully added to array")
+		}
+	}
+	fmt.Println("Room not added to array")
+}
