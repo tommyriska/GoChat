b26433375c52b503b577f442dacacaa5f2f37f67
torasku <tor.arve.skutlaberg@gmail.com>
2017-05-29 19:07:48 +0200

    Added test for adding new server

--- client_test.go
+++ client_test.go
@@ -4,8 +4,6 @@ import (
 	"testing"
 )
 
-/* *** EVERY TEST MUST BE RUN INDIVIDUALLY *** */
-
 /* test will check if a string variable is altered after
 method call (encrypted) */
 func TestEncrypt(t *testing.T) {
@@ -32,3 +30,26 @@ func TestDecrypt(t *testing.T) {
 	}
 }
 
+/* test will check if a new server is added */
+func TestNewServer(t *testing.T) {
+
+	var a string = "testAdr"
+	var p string = "testPort"
+	var n string = "testName"
+	var b bool = false
+
+	storeNewServer(a, p, n)
+
+	dat, err := ioutil.ReadFile("servers.txt")
+	if err != nil {
+		panic(err)
+	}
+	text := string(dat)
+	if strings.Contains(text, a) {
+		fmt.Println("Server added succesfully")
+		b = true
+	}
+	if b == false {
+		fmt.Println("Server not added")
+	}
+}
